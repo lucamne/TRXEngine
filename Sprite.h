5e8c240fc710107ba27b9d9e8b993f8d10ff9309
lucamne <71897009+lucamne@users.noreply.github.com>
2024-02-24 16:11:49 -0500

    Refactoring Sprite class

--- Sprite.h
+++ Sprite.h
@@ -1,28 +1,36 @@
 #pragma once
 
 #include "GLTexture.h"
+#include "SpriteBatch.h"
 
-#include <GL/glew.h>
+#include <glm/glm.hpp>
+
 #include <string>
 
-namespace TRXEngine {
+namespace TRXEngine 
+{
 	class Sprite
 	{
 	public:
-		Sprite();
+		Sprite(glm::vec4 pos_and_dim, const std::string& path_to_texture, glm::vec4 uv_rec = { 0.0f,0.0f,1.0f,1.0f }, float depth = 1.0f, TRXEngine::Color color = { 255,255,255,255 });
 		~Sprite();
 
-		// public member functions
-		void init(float x, float y, float width, float height, std::string texture_path);
-		void draw();
+		void draw(TRXEngine::SpriteBatch& sprite_batch) const;
+
+		// getters
+		glm::vec4 getPositionAndDimensions() const { return m_pos_and_dim; }
+		GLuint getTextureLocation() const { return m_texture.id; }
+
 
 	private:
-		float m_x{};
-		float m_y{};
-		float m_width{};
-		float m_height{};
-		// video buffer id of sprete
-		GLuint m_vbo_id;
-		GLTexture m_texture{};
+		// render depth
+		float m_depth{};
+		// x, y, width, height
+		glm::vec4 m_pos_and_dim{};
+		// all values from 0 to 1, x, y, width, height
+		glm::vec4 m_uv_pos_and_dim{};
+
+		TRXEngine::GLTexture m_texture{};
+		TRXEngine::Color m_color{};
 	};
 }
